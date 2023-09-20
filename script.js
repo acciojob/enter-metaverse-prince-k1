380f331c8db7ad451656fd8adb9228db4d823d29
Prince Kumar Pandit <133173317+prince-k1@users.noreply.github.com>
2023-09-21 01:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+const status = document.getElementById("status");
+const button = document.getElementById("enterBtn");
+const body = document.getElementById("body");
+function enterH(){
+	const heading = document.createElement("h1");
+    heading.textContent = status.textContent;
+    body.insertBefore(heading, button);
+    status.remove();
+    // console.log(body);
+
+    
+}
